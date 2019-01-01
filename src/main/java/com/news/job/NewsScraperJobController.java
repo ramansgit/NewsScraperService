c14c3d4bf3993785029459c1a380694e84bf4beb
ramansgit <sathish.raman36@gmail.com>
2019-01-01 22:48:36 +0530

    Update NewsScraperJobController.java

--- src/main/java/com/news/job/NewsScraperJobController.java
+++ src/main/java/com/news/job/NewsScraperJobController.java
@@ -1,13 +1,29 @@
-package com.news.job;
+package com.news.jobs;
 
-public class NewsScraperJobController implements NewsScraperJob {
+import com.news.exception.PageScrapingException;
+import com.news.scraper.ArticleScrapingHandler;
+import com.news.util.NewsScraperConfig;
+
+public  class NewsScraperJobController implements NewsScraperJob {
 
 	/**
 	 * batch process for fetching articles from given pages
 	 */
+	@Override
 	public void batchProcessor() {
+		ArticleScrapingHandler scrapingHandler = new ArticleScrapingHandler();
 		
-		
-	}
+		try {
+			if(NewsScraperConfig.PAGEONLY_MODE) {
+				scrapingHandler.processPages(NewsScraperConfig.PAGE_TO_PROCESS);
+			}else {
+				scrapingHandler.getArchivedNews();
+			}
+			 
+		} catch (PageScrapingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+	}
 }
