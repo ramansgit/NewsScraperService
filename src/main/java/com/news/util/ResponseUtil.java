6d8c7ff83d2add83da623618e287d67df2b49a0a
ramansgit <sathish.raman36@gmail.com>
2019-01-01 22:43:33 +0530

    Update ResponseUtil.java

--- src/main/java/com/news/util/ResponseUtil.java
+++ src/main/java/com/news/util/ResponseUtil.java
@@ -4,85 +4,40 @@ package com.news.util;
 public class ResponseUtil {
 
 	/**
-	 * handle all type of exception
+	 * handle all type of exceptions
+	 * 
 	 * @param e
 	 * @return
 	 */
-	public static Response handleException(Exception e) {
-		if (e instanceof ValidationException) {
-			e.printStackTrace();
-			return Response.ok().status(Status.BAD_REQUEST)
-					.entity(new ApiResponseMessage(new ApiError(AccountMgmtConstant.BAD_REQUEST, e.getMessage())))
-					.build();
-		}
-		if (e instanceof UnAuthorizedAccessException) {
-			e.printStackTrace();
-			return Response.ok().status(Status.UNAUTHORIZED).entity(
-					new ApiResponseMessage(new ApiError(AccountMgmtConstant.UNAUTHORISED_ACCESS, e.getMessage())))
-					.build();
-		}
-		if (e instanceof MongoDatabaseException) {
-			e.printStackTrace();
-			return Response.ok().status(Status.INTERNAL_SERVER_ERROR)
-					.entity(new ApiResponseMessage(new ApiError(AccountMgmtConstant.DB_EXCEPTION, e.getMessage())))
-					.build();
-		}
+	public static ApiResponse handleException(Exception e) {
+		if (e instanceof PageScrapingException) {
+			return new ApiResponse(NewsScraperConfig.PAGE_SCRAPING_EXCEPTION, Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+					StatusResponse.ERROR, e.getMessage());
 
-	
-		if (e instanceof JWTIssuerException) {
-			e.printStackTrace();
-			return Response.ok().status(Status.INTERNAL_SERVER_ERROR).entity(
-					new ApiResponseMessage(new ApiError(AccountMgmtConstant.JWT_TOKEN_EXCEPTION, e.getMessage())))
-					.build();
 		}
-		if (e instanceof EmailException) {
-			e.printStackTrace();
-			return Response.ok().status(Status.INTERNAL_SERVER_ERROR)
-					.entity(new ApiResponseMessage(new ApiError(AccountMgmtConstant.EMAIL_EXCEPTION, e.getMessage())))
-					.build();
-		}
+		if (e instanceof DocumentSearchException) {
+			return new ApiResponse(NewsScraperConfig.DOCUMENT_SEARCHING_EXCEPTION, Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+					StatusResponse.ERROR, e.getMessage());
 
-		if (e instanceof Exception) {
-			e.printStackTrace();
-			return Response.ok().status(Status.INTERNAL_SERVER_ERROR)
-					.entity(new ApiResponseMessage(new ApiError(AccountMgmtConstant.GENERIC_EXCEPTION, e.getMessage())))
-					.build();
 		}
-		return Response.ok().status(Status.INTERNAL_SERVER_ERROR)
-				.entity(new ApiResponseMessage(new ApiError(AccountMgmtConstant.GENERIC_EXCEPTION, e.getMessage())))
-				.build();
+		if (e instanceof DocumentIndexingException) {
+			return new ApiResponse(NewsScraperConfig.DOCUMENT_INDEXING_EXCEPTION, Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+					StatusResponse.ERROR, e.getMessage());
+
+		}
+		return new ApiResponse(NewsScraperConfig.GENERIC_EXCEPTION, Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+				StatusResponse.ERROR, e.getMessage());
 
 	}
 
 	/**
-	 *
+	 * returns success response
+	 * 
 	 * @param data
 	 * @return
 	 */
-	public static Response handleSuccessResp(Object data) {
-		return Response.ok().status(Status.OK).entity(new ApiResponseMessage(data)).build();
-
-	}
-
-	/**
-	 *
-	 * @param message
-	 * @return
-	 */
-	public static Response handleSuccessResp(String message) {
-		return Response.ok().status(Status.OK).entity(new MsgSuccess(message)).build();
-
-	}
-
-	/**
-	 *
-	 * @param status
-	 * @param code
-	 * @param msg
-	 * @return
-	 */
-	public static Response handleFailureResp(Status status, String code, String msg) {
-		return Response.ok().status(status).entity(new ApiResponseMessage(new ApiError(code, msg))).build();
+	public static ApiResponse handleSuccessResp(Object data) {
+		return new ApiResponse(NewsScraperConfig.SUCCESS, StatusResponse.SUCCESS, Status.OK.getStatusCode(), data);
 
 	}
 }
