44709395d0bc0291cd0bc90481f62817825108af
ramansgit <sathish.raman36@gmail.com>
2019-01-01 22:42:54 +0530

    Update NewsScraperConfig.java

--- src/main/java/com/news/util/NewsScraperConfig.java
+++ src/main/java/com/news/util/NewsScraperConfig.java
@@ -1,5 +1,15 @@
 package com.news.util;
 
 public class NewsScraperConfig {
-
+// code
+	public static final String GENERIC_EXCEPTION = "GENERIC_EXCEPTION";
+	public static final String PAGE_SCRAPING_EXCEPTION = "PAGE_SCRAPING_EXCEPTION";
+	public static final String DOCUMENT_INDEXING_EXCEPTION = "DOCUMENT_INDEXING_EXCEPTION";
+	public static final String DOCUMENT_SEARCHING_EXCEPTION = "DOCUMENT_SEARCHING_EXCEPTION";
+	public static final String SUCCESS = "SUCCESS";
+	public static final String SOLR_ENDPOINT = "http://localhost:8983/solr/articles_core";
+	
+	//page to process
+	public static final String PAGE_TO_PROCESS="https://www.thehindu.com/archive/web/2018/12/30/";
+	public static final boolean PAGEONLY_MODE=true;
 }
