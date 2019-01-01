4ba8420dee1e6f297391bb72a23a2b16043903a4
ramansgit <sathish.raman36@gmail.com>
2019-01-01 22:47:53 +0530

    Update NewsScraperSearchController.java

--- src/main/java/com/news/api/NewsScraperSearchController.java
+++ src/main/java/com/news/api/NewsScraperSearchController.java
@@ -1,20 +1,63 @@
 package com.news.api;
 
+import com.google.gson.Gson;
+import com.news.model.ApiResponse;
+import com.news.search.SearchHandler;
+import com.news.util.ResponseUtil;
+
+/**
+ * 
+ * NewsScraperSearchController for getting search results
+ *
+ */
 public class NewsScraperSearchController implements NewsScraperSearchApi{
-
-	public String getAuthorList(int start, int pageSize) {
-		// TODO Auto-generated method stub
-		return null;
+	private static SearchHandler instance =  null;
+	public NewsScraperSearchController() {
+		instance = SearchHandler.getInstance();
+	}
+	/**
+	 * fetches list of authors
+	 */
+	@Override
+	public String getAuthorsList(int start, int pageSize) {
+		ApiResponse resp = null;
+		try {
+			resp = ResponseUtil.handleSuccessResp(instance.getPaginatedAuthorList(start, pageSize));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Gson().toJson(ResponseUtil.handleException(e));
+		}
+		return new Gson().toJson(resp);
 	}
 
-	public String getArticlesForAuthor(String authorName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * fetches articles by title and description
+	 */
+	@Override
 	public String getArticlesByTitleAndDescription(String title, String description) {
-		// TODO Auto-generated method stub
-		return null;
+		ApiResponse resp = null;
+		try {
+			resp = ResponseUtil.handleSuccessResp(instance.filterByTitleAndDescription(title, description));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Gson().toJson(ResponseUtil.handleException(e));
+		}
+		return new Gson().toJson(resp);
+	}
+
+	/**
+	 * fetches articles for given author
+	 */
+	@Override
+	public String getArticlesForAuthor(String authorName) {
+		ApiResponse resp = null;
+		try {
+			resp = ResponseUtil.handleSuccessResp(instance.filterByAuthor(authorName));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Gson().toJson(ResponseUtil.handleException(e));
+		}
+		return new Gson().toJson(resp);
 	}
 
 }
